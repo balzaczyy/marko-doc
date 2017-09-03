676c3345f50cfff7c2ea3701e5c7772f5a514b39
balzaczyy <balzaczyy@gmail.com>
2017-09-03 10:51:51 +0800

    fix test

--- src/components/color-picker/util/getColors.js
+++ src/components/color-picker/util/getColors.js
@@ -5,7 +5,8 @@ const flatColors = require('flat-colors').colors;
 
 const HEX_INDEX = 3;
 
-/* export default */ module.exports = function getColors () {
+// export default function getColors () {
+module.exports = function getColors () {
   let colors = [];
   for (let i = 0; i < 10; i++) {
     colors.push(flatColors[i][HEX_INDEX]);
